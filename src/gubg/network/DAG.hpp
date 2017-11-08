d650ef6ab07208fe6fa27b37a496f01e1bddc2c3
Geert Fannes <geert.fannes@auro-technologies.com>
2017-11-09 00:47:29 +0100

    The DAG keeps the topological order continuously up-to-date

--- src/gubg/network/DAG.hpp
+++ src/gubg/network/DAG.hpp
@@ -3,69 +3,156 @@
 
 #include "gubg/mss.hpp"
 #include <set>
-#include <vector>
+#include <map>
+#include <list>
 #include <cassert>
 
 namespace gubg { namespace network { 
 
-    template <typename T>
+    enum class Direction { Forward, Backward, };
+
+    template <typename Vertex>
     class DAG
     {
     public:
-        using Self = DAG<T>;
-        using Vertices = std::set<T*>;
+        using Self = DAG<Vertex>;
+        using Sequence = std::list<Vertex*>;
+        using Set = std::set<Vertex*>;
+        struct Info
+        {
+            int order = 0;
+            typename Sequence::iterator it;
+            Set dsts;
+            Info(){}
+            Info(int order): order(order){}
+        };
+        using InfoPerVertex = std::map<Vertex*, Info>;
 
         void clear(){*this = Self{};}
 
-        bool add_vertex(T *v)
+        size_t size() const
         {
-            MSS_BEGIN(bool);
+            assert(invariants_());
+            return sequence_.size();
+        }
+
+        bool add_vertex(Vertex *v)
+        {
+            MSS_BEGIN(bool, "");
+            L(C(*v));
+            assert(invariants_());
             MSS(!!v);
-            MSS(vertices_.insert(v).second);
+            int order = -(int)size();
+            auto it = info_.insert(std::make_pair(v, Info{order}));
+            MSS(it.second);
+            sequence_.push_front(v);
+            it.first->second.it = sequence_.begin();
+            assert(invariants_());
             MSS_END();
         }
 
-        bool add_edge(T *src, T *dst)
+        bool add_edge(Vertex *src, Vertex *dst)
         {
-            MSS_BEGIN(bool);
-            MSS(!!src && !!dst);
-            MSS(out_edges_[src].insert(dst).second);
+            MSS_BEGIN(bool, "");
+            L(C(*src)C(*dst));
+            assert(invariants_());
+
+            MSS(src != dst);
+
+            auto src_it = info_.find(src); MSS(src_it != info_.end());
+            auto dst_it = info_.find(dst); MSS(dst_it != info_.end());
+
+            if (src_it->second.order < dst_it->second.order)
+            {
+                L("Nodes are correctly ordered");
+                MSS(src_it->second.dsts.insert(dst).second);
+            }
+            else
+            {
+                L("Dst must be moved after src");
+                //Check if all dsts are beyond the src order we have to mode dst to
+                {
+                    const auto src_order = src_it->second.order;
+                    for (auto v: dst_it->second.dsts)
+                    {
+                        MSS(src_order < info_[v].order);
+                    }
+                }
+                
+                {
+                    //Remove dst and record the iterator after it: we have to update the order starting from there lateron
+                    auto start_it = sequence_.erase(dst_it->second.it);
+                    //Get the iterator after the location where dst should be inserted
+                    auto it = src_it->second.it; ++it;
+                    //Insert dst
+                    auto new_dst_it = sequence_.insert(it, dst);
+                    //Update the dst info
+                    dst_it->second.it = new_dst_it;
+                    dst_it->second.order = src_it->second.order;
+                    //Update the order for the vertices affected by this move in the sequence
+                    //This is expensive since we have to look-up each info in the map
+                    for (auto it = start_it; it != new_dst_it; ++it)
+                        --info_[*it].order;
+                    //Added the edge from src to dst
+                    MSS(src_it->second.dsts.insert(dst).second);
+                }
+            }
+            assert(invariants_());
             MSS_END();
         }
 
-        template <typename Ftor>
+        template <Direction dir, typename Ftor>
         bool each_vertex(Ftor ftor)
         {
             MSS_BEGIN(bool);
-            for (auto v: vertices_)
+            switch (dir)
             {
-                MSS(ftor(v));
+                case Direction::Forward:
+                    for (auto v: sequence_) { MSS(ftor(v)); }
+                    break;
+                case Direction::Backward:
+                    for (auto it = sequence_.rbegin(); it != sequence_.rend(); ++it)
+                    {
+                        MSS(ftor(*it));
+                    }
+                    break;
             }
             MSS_END();
         }
-        template <typename Ftor>
+        template <Direction dir, typename Ftor>
         bool each_vertex(Ftor ftor) const
         {
             MSS_BEGIN(bool);
-            for (auto v: vertices_)
+            switch (dir)
             {
-                MSS(ftor(v));
+                case Direction::Forward:
+                    for (auto v: sequence_) { MSS(ftor(v)); }
+                    break;
+                case Direction::Backward:
+                    for (auto it = sequence_.rbegin(); it != sequence_.rend(); ++it)
+                    {
+                        MSS(ftor(*it));
+                    }
+                    break;
             }
             MSS_END();
         }
 
-        bool remove_unreachables(T *v)
+        bool remove_unreachables(Vertex *v)
         {
-            MSS_BEGIN(bool);
-            MSS(vertices_.count(v) > 0);
+            MSS_BEGIN(bool, "");
+            MSS(info_.count(v) > 0);
+
+            using Vertices = std::set<Vertex*>;
             Vertices pruned;
             Vertices stage = {v}, new_stage;
+
             while (!stage.empty())
             {
                 assert(new_stage.empty());
 
                 for (auto src: stage)
-                    for (auto dst: out_edges_[src])
+                    for (auto dst: info_[src].dsts)
                     {
                         MSS(pruned.count(dst) == 0);
                         MSS(stage.count(dst) == 0);
@@ -82,14 +169,53 @@ namespace gubg { namespace network {
 
                 stage.swap(new_stage);
             }
-            vertices_.swap(pruned);
+
+            int order = -(int)pruned.size();
+            for (auto p = info_.begin(); p != info_.end();)
+            {
+                if (pruned.count(p->first) > 0)
+                {
+                    //This vertex should be kept, we only need to update the order
+                    p->second.order = ++order;
+                    ++p;
+                }
+                else
+                {
+                    //This vertex should be removed
+                    sequence_.erase(p->second.it);
+                    p = info_.erase(p); 
+                }
+            }
+
             MSS_END();
         }
 
     private:
-        Vertices vertices_;
-        using EdgesPerVertex = std::map<T*, Vertices>;
-        EdgesPerVertex out_edges_;
+        bool invariants_() const
+        {
+            MSS_BEGIN(bool, "");
+            MSS(sequence_.size() == info_.size());
+            for (const auto &p: info_)
+            {
+                MSS(p.first == *p.second.it);
+            }
+            int order = sequence_.size();
+            order = -order;
+            for (auto it = sequence_.begin(); it != sequence_.end(); ++it)
+            {
+                L(**it);
+                ++order;
+                auto p = info_.find(*it);
+                MSS(p != info_.end());
+                L(C(p->second.order)C(order));
+                MSS(p->second.order == order);
+                MSS(p->second.it == it);
+            }
+            MSS_END();
+        }
+
+        Sequence sequence_;
+        InfoPerVertex info_;
     };
 
 } } 
---
+++ src/test/network/DAG_tests.cpp
@@ -0,0 +1,75 @@
+#include "catch.hpp"
+#include "gubg/network/DAG.hpp"
+
+TEST_CASE("gubg::network::DAG tests", "[ut][dag]")
+{
+    using Vertex = std::string;
+    using DAG = gubg::network::DAG<Vertex>;
+
+    Vertex a = "a";
+    Vertex b = "b";
+    Vertex c = "c";
+
+    DAG dag;
+
+    REQUIRE(dag.size() == 0);
+
+    REQUIRE(dag.add_vertex(&a));
+    REQUIRE(dag.size() == 1);
+
+    dag.clear();
+    REQUIRE(dag.size() == 0);
+
+    REQUIRE(dag.add_vertex(&a));
+    REQUIRE(dag.size() == 1);
+
+    REQUIRE(!dag.add_vertex(&a));
+    REQUIRE(dag.size() == 1);
+
+    REQUIRE(dag.add_vertex(&b));
+
+
+    REQUIRE(!dag.add_edge(&a, &a));
+    REQUIRE(dag.add_edge(&a, &b));
+    REQUIRE(!dag.add_edge(&a, &b));
+    REQUIRE(!dag.add_edge(&b, &a));
+    REQUIRE(!dag.add_edge(&a, &c));
+
+    REQUIRE(dag.add_vertex(&c));
+    REQUIRE(dag.add_edge(&a, &c));
+    REQUIRE(dag.add_edge(&b, &c));
+
+    {
+        char ch = 'a';
+        auto check = [&](Vertex *v){
+            MSS_BEGIN(bool);
+            MSS((*v)[0] == ch);
+            ++ch;
+            MSS_END();
+        };
+        REQUIRE(dag.each_vertex<gubg::network::Direction::Forward>(check));
+    }
+    {
+        char ch = 'c';
+        auto check = [&](Vertex *v){
+            MSS_BEGIN(bool);
+            MSS((*v)[0] == ch);
+            --ch;
+            MSS_END();
+        };
+        REQUIRE(dag.each_vertex<gubg::network::Direction::Backward>(check));
+    }
+
+    REQUIRE(dag.remove_unreachables(&b));
+    REQUIRE(dag.size() == 2);
+    {
+        char ch = 'b';
+        auto check = [&](Vertex *v){
+            MSS_BEGIN(bool);
+            MSS((*v)[0] == ch);
+            ++ch;
+            MSS_END();
+        };
+        REQUIRE(dag.each_vertex<gubg::network::Direction::Forward>(check));
+    }
+}
