2be5591125b5bd801014741ec82591bf43456c02
Geert Fannes <geert.fannes@gmail.com>
2017-12-31 13:04:20 +0100

    wip

--- src/gubg/optimization/SCG.hpp
+++ src/gubg/optimization/SCG.hpp
@@ -1,6 +1,8 @@
 #ifndef HEADER_gubg_optimization_SCG_hpp_ALREADY_INCLUDED
 #define HEADER_gubg_optimization_SCG_hpp_ALREADY_INCLUDED
 
+#include <cmath>
+
 namespace gubg { namespace optimization { 
 
     namespace details { 
@@ -9,23 +11,40 @@ namespace gubg { namespace optimization {
         };
     } 
 
-    template <typename Float, typename Param, typename Outer = details::Default>
+    template <typename Float, typename Params, typename Outer = details::Default>
     class SCG
     {
     public:
         SCG(): outer_(default_outer_) {}
         SCG(Outer &outer): outer_(outer) {}
 
-        void operator()()
+        template <typename Function, typename Gradient>
+        void operator()(const typename Params::Type &params, const Function &function, const Gradient &gradient)
         {
+            //1. Choose initial parameters
             sigma_ = 0.001;
-            k_ = 0;
+
+            w_ = params;
+            r_ = params;
+
+            gradient(r_, w_);
+            p_ = r_;
+
+            unsigned int k = 0;
+            bool success = true;
+
+            //2. Calculate second order information
+            if (success)
+            {
+                sigma_k_ = sigma_/std::sqrt(Params::sum_squares(p_));
+            }
         }
 
     private:
         Float sigma_;
-        unsigned int k_;
-        typename Param::Type w_;
+        Float sigma_k_;
+        typename Params::Type w_, p_, r_;
+        typename Params::Type s_;
 
         details::Default default_outer_;
         Outer &outer_;
--- src/test/optimization/SCG_tests.cpp
+++ src/test/optimization/SCG_tests.cpp
@@ -1,22 +1,49 @@
 #include "catch.hpp"
 #include "gubg/optimization/SCG.hpp"
 #include <vector>
+#include <cassert>
 using namespace gubg;
 
 namespace  { 
     using Float = double;
 
-    struct Param
+    struct Params
     {
         using Type = std::vector<Float>;
+        static Float sum_squares(const Type &params)
+        {
+            Float ss = 0.0;
+            for (auto v: params)
+                ss += v*v;
+            return ss;
+        }
+    };
+    struct Function
+    {
+        Float operator()(const Params::Type &params) const
+        {
+            return Params::sum_squares(params)/2.0;
+        }
+    };
+    struct Gradient
+    {
+        void operator()(Params::Type &gradient, const Params::Type &params) const
+        {
+            assert(gradient.size() == params.size());
+            gradient = params;
+        }
     };
 } 
 
 TEST_CASE("optimization::SCG tests without outer", "[ut][scg]")
 {
-    optimization::SCG<Float, Param> scg;
+    optimization::SCG<Float, Params> scg;
 
-    scg();
+    Params::Type params;
+    Function f;
+    Gradient g;
+
+    scg(params, f, g);
 }
 
 namespace  { 
@@ -27,7 +54,11 @@ namespace  {
 TEST_CASE("optimization::SCG tests with outer", "[ut][scg]")
 {
     Outer outer;
-    optimization::SCG<Float, Param, Outer> scg(outer);
+    optimization::SCG<Float, Params, Outer> scg(outer);
 
-    scg();
+    Params::Type params;
+    Function f;
+    Gradient g;
+
+    scg(params, f, g);
 }
