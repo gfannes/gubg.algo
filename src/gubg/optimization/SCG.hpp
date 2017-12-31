7a768f3608d588de66ff192a2621dc18c789b696
Geert Fannes <geert.fannes@gmail.com>
2017-12-31 16:02:52 +0100

    Something seems to be working for a squared function

--- src/gubg/optimization/SCG.hpp
+++ src/gubg/optimization/SCG.hpp
@@ -1,52 +1,192 @@
 #ifndef HEADER_gubg_optimization_SCG_hpp_ALREADY_INCLUDED
 #define HEADER_gubg_optimization_SCG_hpp_ALREADY_INCLUDED
 
+#include "gubg/debug.hpp"
+#include <array>
 #include <cmath>
+#include <cassert>
 
 namespace gubg { namespace optimization { 
 
     namespace details { 
-        struct Default
+        struct DoNothing
         {
+            template <typename Float ,typename Params>
+            void scg_params(unsigned int iteration, Float eval, const Params &params) {}
+
+            template <typename Float ,typename Params>
+            bool scg_terminate(unsigned int iteration, Float eval, const Params &gradient)
+            {
+                for (auto g: gradient)
+                    if (std::abs(g) > 0.000000001)
+                        return false;
+                return true;
+            }
         };
     } 
 
-    template <typename Float, typename Params, typename Outer = details::Default>
+    template <typename Float, typename Params, typename Outer = details::DoNothing>
     class SCG
     {
     public:
-        SCG(): outer_(default_outer_) {}
+        SCG(): outer_(do_nothing_outer_) {}
         SCG(Outer &outer): outer_(outer) {}
 
         template <typename Function, typename Gradient>
-        void operator()(const typename Params::Type &params, const Function &function, const Gradient &gradient)
+        Float operator()(typename Params::Type &params, const Function &function, const Gradient &gradient)
         {
-            //1. Choose initial parameters
-            sigma_ = 0.001;
+            S("");
 
-            w_ = params;
-            r_ = params;
-
-            gradient(r_, w_);
-            p_ = r_;
-
-            unsigned int k = 0;
-            bool success = true;
-
-            //2. Calculate second order information
-            if (success)
+            L("1. Initialization");
             {
-                sigma_k_ = sigma_/std::sqrt(Params::sum_squares(p_));
+                setup_(params);
+
+                auto &h0 = history_data_[iteration_&1u];
+                gradient(h0.r, h0.w);
+                h0.p = h0.r;
+
+                h0.eval = function(h0.w);
+            }
+
+            for (; true; ++iteration_)
+            {
+                auto &h0 = history_data_[iteration_&1u];
+                auto &h1 = history_data_[(iteration_+1)&1u];
+
+                outer_.scg_params(iteration_, h0.eval, h0.w);
+
+                L("2. Calculate second order information");
+                h0.ss_p = Params::sum_squares(h0.p);
+                if (success_)
+                {
+                    const Float sigma_k = sigma_/std::sqrt(h0.ss_p);
+                    L(C(sigma_k));
+                    h1.w = h0.w;
+                    Params::update(h1.w, sigma_k, h0.p);
+                    gradient(h1.r, h1.w);
+                    Params::update(h1.r, -1.0, h0.r);
+                    delta_k_ = -Params::inprod(h0.p, h1.r)/sigma_k;
+                    L(C(delta_k_));
+                }
+
+                L("3. Scale");
+                delta_k_ += (lambda_k_-lambda_bar_k_)*h0.ss_p;
+                L(C(delta_k_));
+
+                L("4. Make Hessian positive definite");
+                if (delta_k_ <= 0)
+                {
+                    lambda_bar_k_ = 2.0*(lambda_k_-delta_k_/h0.ss_p);
+                    delta_k_ = -delta_k_ + lambda_k_*h0.ss_p;
+                    lambda_k_ = lambda_bar_k_;
+                    L(C(delta_k_)C(lambda_k_)C(lambda_bar_k_));
+                }
+                assert(delta_k_ >= 0);
+
+                L("5. Calculate step size");
+                const Float mu_k = Params::inprod(h0.p, h0.r);
+                const Float alpha_k = mu_k/delta_k_;
+                L(C(mu_k)C(alpha_k));
+
+                L("6. Comparison parameter");
+                h1.w = h0.w;
+                Params::update(h1.w, alpha_k, h0.p);
+                h1.eval = function(h1.w);
+                const Float diff_k = 2.0*delta_k_*(h1.eval - h0.eval)/(mu_k*mu_k);
+                L(C(diff_k));
+
+                L("7. Reduce error, if possible");
+                if (diff_k >= 0.0)
+                {
+                    //h1.w is already correct from step 6
+                    gradient(h1.r, h1.w);
+                    lambda_bar_k_ = 0.0;
+                    success_ = true;
+                    if ((iteration_+1)%order_ == 0)
+                    {
+                        L("Restarting algorithm");
+                        h1.p = h1.r;
+                    }
+                    else
+                    {
+                        L("Continuing ...");
+                        const Float beta_k = (Params::sum_squares(h1.r)-Params::inprod(h1.r, h0.r))/mu_k;
+                        h1.p = h1.r;
+                        Params::update(h1.p, beta_k, h0.p);
+                    }
+                    if (diff_k >= 0.75)
+                    {
+                        L("Reducing the scale parameter");
+                        lambda_k_ *= 0.25;
+                    }
+                }
+                else
+                {
+                    lambda_bar_k_ = lambda_k_;
+                    success_ = false;
+                }
+
+                L("8. Increase scale, if needed");
+                if (diff_k < 0.25)
+                {
+                    L("Increasing the scale parameter");
+                    lambda_k_ += delta_k_*(1.0-diff_k)/h0.ss_p;
+                }
+
+                L("9. Check for end");
+                if (outer_.scg_terminate(iteration_, h0.eval, h0.r))
+                {
+                    L("We found a suitable maximum in iteration " << iteration_ << ": " << h0.eval);
+                    params = h0.w;
+                    return h0.eval;
+                }
             }
         }
 
     private:
-        Float sigma_;
-        Float sigma_k_;
-        typename Params::Type w_, p_, r_;
-        typename Params::Type s_;
+        void setup_(const typename Params::Type &params)
+        {
+            S("");
 
-        details::Default default_outer_;
+            order_ = Params::order(params);
+            L(C(order_));
+
+            iteration_ = 0;
+
+            success_ = true;
+
+            auto setup_history = [&](unsigned int t)
+            {
+                history_data_[t].w = params;
+                history_data_[t].r = params;
+                history_data_[t].p = params;
+            };
+            setup_history(0);
+            setup_history(1);
+
+            sigma_ = 0.001;
+            lambda_k_ = 0.00001;
+            lambda_bar_k_ = 0.0;
+            L(C(sigma_)C(lambda_k_)C(lambda_bar_k_));
+        }
+
+        unsigned int order_;
+        unsigned int iteration_;
+        bool success_ = true;
+        Float sigma_;
+
+        struct Data
+        {
+            typename Params::Type w, r, p;
+            Float ss_r, ss_p;
+            Float eval;
+        };
+        Data history_data_[2];
+
+        Float delta_k_;
+        Float lambda_k_, lambda_bar_k_;
+
+        details::DoNothing do_nothing_outer_;
         Outer &outer_;
     };
 
--- src/test/optimization/SCG_tests.cpp
+++ src/test/optimization/SCG_tests.cpp
@@ -2,6 +2,7 @@
 #include "gubg/optimization/SCG.hpp"
 #include <vector>
 #include <cassert>
+#include <iostream>
 using namespace gubg;
 
 namespace  { 
@@ -10,6 +11,10 @@ namespace  {
     struct Params
     {
         using Type = std::vector<Float>;
+        static unsigned int order(const Type &params)
+        {
+            return params.size();
+        }
         static Float sum_squares(const Type &params)
         {
             Float ss = 0.0;
@@ -17,12 +22,37 @@ namespace  {
                 ss += v*v;
             return ss;
         }
+        static Float inprod(const Type &a, const Type &b)
+        {
+            assert(a.size() == b.size());
+            Float res = 0.0;
+            const auto size = a.size();
+            for (size_t ix = 0; ix < size; ++ix)
+                res += a[ix]*b[ix];
+            return res;
+        }
+        static void update(Type &dst, Float factor, const Type &src)
+        {
+            auto it = dst.begin();
+            for (auto v: src)
+                *it++ += factor*v;
+        }
     };
+    void print(const Params::Type &params)
+    {
+        std::cout << "[";
+        for (auto v: params)
+            std::cout << v << " ";
+        std::cout << "]";
+    }
+
     struct Function
     {
         Float operator()(const Params::Type &params) const
         {
-            return Params::sum_squares(params)/2.0;
+            const Float v = -Params::sum_squares(params);
+            std::cout << "Function in "; print(params); std::cout << " is " << v << std::endl;
+            return v;
         }
     };
     struct Gradient
@@ -31,15 +61,18 @@ namespace  {
         {
             assert(gradient.size() == params.size());
             gradient = params;
+            for (auto &v: gradient)
+                v = -2.0*v;
+            std::cout << "Gradient in "; print(params); std::cout << " is "; print(gradient); std::cout << std::endl;
         }
     };
 } 
 
-TEST_CASE("optimization::SCG tests without outer", "[ut][scg]")
+TEST_CASE("optimization::SCG tests without outer", "[ut][scg_]")
 {
     optimization::SCG<Float, Params> scg;
 
-    Params::Type params;
+    Params::Type params = {1.0, 2.0};
     Function f;
     Gradient g;
 
@@ -49,6 +82,18 @@ TEST_CASE("optimization::SCG tests without outer", "[ut][scg]")
 namespace  { 
     struct Outer
     {
+        void scg_params(unsigned int iteration, Float eval, const Params::Type &params)
+        {
+            std::cout << std::endl << "Iteration " << iteration << ": " << eval << " @ ";
+            print(params);
+            std::cout << std::endl;
+        }
+        bool scg_terminate(unsigned int iteration, Float eval, const Params::Type &gradient)
+        {
+            const auto ss_gradient = Params::sum_squares(gradient);
+            std::cout << "Gradient: "; print(gradient); std::cout << ", ss: " << ss_gradient << std::endl;
+            return ss_gradient <= 0.0001;
+        }
     };
 } 
 TEST_CASE("optimization::SCG tests with outer", "[ut][scg]")
@@ -56,7 +101,7 @@ TEST_CASE("optimization::SCG tests with outer", "[ut][scg]")
     Outer outer;
     optimization::SCG<Float, Params, Outer> scg(outer);
 
-    Params::Type params;
+    Params::Type params = {1.0, 2.0};
     Function f;
     Gradient g;
 
