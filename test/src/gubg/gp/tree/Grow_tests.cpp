542277aedb2ee612c279b1a2071ab3ca54f2391f
Geert Fannes <geert.fannes@auro-technologies.com>
2019-11-28 16:33:34 +0100

    Created 100 trees

--- test/src/gubg/gp/tree/Grow_tests.cpp
+++ test/src/gubg/gp/tree/Grow_tests.cpp
@@ -16,22 +16,27 @@ TEST_CASE("gp::tree::grow tests", "[ut][gp][tree][Grow]")
     tree::test::Plus plus;
     functions.push_back(tree::create_function<Node>(plus));
 
-    NodePtr root;
-    {
-        tree::Grow<Node> grow;
-        REQUIRE(!grow(root, terminals, functions));
-        REQUIRE(grow.set_probs(0.1,1));
-        REQUIRE(!grow(root, terminals, functions));
-        REQUIRE(grow.set_max_depth(3));
-        REQUIRE(grow(root, terminals, functions));
-    }
+    tree::Grow<Node> grow;
+    REQUIRE(grow.set_probs(1,1));
+    REQUIRE(grow.set_max_depth(3));
 
-    int level = -1;
+    std::vector<NodePtr> trees(100);
+    for (auto &tree: trees)
+    {
+        REQUIRE(grow(tree, terminals, functions));
+    }
+    std::cout << "All trees are grown" << std::endl;
+
+    int level = 0;
     typename Node::Functor print = [&](const Node::Base *base, tree::DFS dfs)
     {
-        level += (dfs == tree::DFS::Open);
-        std::cout << std::string(2*level, ' ') << base->hr() << std::endl;
         level -= (dfs == tree::DFS::Close);
+        std::cout << std::string(2*level, ' ') << base->hr() << std::endl;
+        level += (dfs == tree::DFS::Open);
     };
-    root->dfs(print);
+    for (auto &tree: trees)
+    {
+        std::cout << tree.get() << std::endl;
+        tree->dfs(print);
+    }
 }
